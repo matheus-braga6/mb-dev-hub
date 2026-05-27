a82fb4851be29d1212add58e025ba896b552f877
Matheus Braga <matheus-braga6@hotmail.com>
2026-05-27 00:14:50 -0300

    docs(certificates): add new certification

--- src/data/certifications.ts
+++ src/data/certifications.ts
@@ -2,6 +2,24 @@ import type { CertificationImage } from "@/types/certifications"
 
 export const certifications: CertificationImage[] = [
   {
+    id: "typescript-scrimba",
+    image: "./certifications/typescript.webp",
+    alt: "Learn TypeScript",
+    link: "https://scrimba.com/learn-typescript-c03c"
+  },
+  {
+    id: "b1english-freecodecamp",
+    image: "./certifications/b1-english.webp",
+    alt: "B1 English for Developers",
+    link: "https://www.freecodecamp.org/certification/braga/b1-english-for-developers"
+  },
+  {
+    id: "a2english-freecodecamp",
+    image: "./certifications/a2-english.webp",
+    alt: "A2 English for Developers",
+    link: "https://www.freecodecamp.org/certification/braga/a2-english-for-developers"
+  },
+  {
     id: "react-scrimba",
     image: "./certifications/react.webp",
     alt: "React Certification",
@@ -18,17 +36,5 @@ export const certifications: CertificationImage[] = [
     image: "./certifications/responsive-web-design.webp",
     alt: "Responsive Web Design",
     link: "https://www.freecodecamp.org/certification/braga/responsive-web-design"
-  },
-  {
-    id: "a2english-freecodecamp",
-    image: "./certifications/a2-english.webp",
-    alt: "A2 English for Developers",
-    link: "https://www.freecodecamp.org/certification/braga/a2-english-for-developers"
-  },
-  {
-    id: "b1english-freecodecamp",
-    image: "./certifications/b1-english.webp",
-    alt: "B1 English for Developers",
-    link: "https://www.freecodecamp.org/certification/braga/b1-english-for-developers"
   }
 ]
